c9e60de0bd93d7c1186a17f6946ca68e6e14c5a3
Lee Connelly <leeconnelly12@gmail.com>
2020-02-29 17:36:28 +0000

    initital commit

---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+const mix = require('laravel-mix')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+mix.postCss('css/app.css', 'dist', [
+  require('tailwindcss'),
+  ...mix.inProduction() ? [
+    purgecss({
+      content: ['index.html'],
+      css: ['css/app.css'],
+      defaultExtractor: content => content.match(/[\w-/:.]+(?<!:)/g) || [],
+    }),
+  ] : [],
+])
+  .version()
+  .sourceMaps()
+  .setPublicPath('dist')
