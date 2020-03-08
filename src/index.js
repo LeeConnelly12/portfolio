8a1cd362c6befcee4d3bcb6e89f9b55e876ef403
Lee Connelly <leeconnelly12@gmail.com>
2020-03-08 23:47:10 +0000

    added more projects

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import './style.css'
 import Atoz from './images/projects/atoz.jpg'
 import Stadium from './images/projects/stadium.jpg'
 import GasData from './images/projects/gasdata.jpg'
+import Fitzgerald from './images/projects/fitzgerald.jpg'
+import ClubGlobal from './images/projects/clubglobal.jpg'
+import Robinsandday from './images/projects/robinsandday.jpg'
 
 document.getElementById('atoz').src = Atoz
 document.getElementById('stadium').src = Stadium
 document.getElementById('gasData').src = GasData
+document.getElementById('fitzgerald').src = Fitzgerald
+document.getElementById('clubGlobal').src = ClubGlobal
+document.getElementById('robinsandday').src = Robinsandday
 
 document.getElementById('hamburger').addEventListener('click', function() {
   document.getElementById(this.dataset.opens).classList.toggle('hidden')
