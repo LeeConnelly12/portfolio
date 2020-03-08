bb877c78c4d2c391763c27e5be23281c01e57ae0
Lee Connelly <leeconnelly12@gmail.com>
2020-03-08 15:59:34 +0000

    made site responsive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css'
+import Atoz from './images/projects/atoz.jpg'
+import Stadium from './images/projects/stadium.jpg'
+import GasData from './images/projects/gasdata.jpg'
+
+document.getElementById('atoz').src = Atoz
+document.getElementById('stadium').src = Stadium
+document.getElementById('gasData').src = GasData
--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,48 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HtmlCriticalPlugin = require("html-critical-webpack-plugin")
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-module.exports = (env, argv) => {
-  return {
-    entry: './src/index.js',
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: 'src/index.html',
-        inject: argv.mode === 'development'
-      }),
-      new MiniCssExtractPlugin(),
-      ...argv.mode === 'production' ? [new HtmlCriticalPlugin({
-        base: path.join(path.resolve(__dirname), 'dist/'),
-        src: 'index.html',
-        dest: 'index.html',
-        inline: true,
-        minify: true,
-      })] : []
-    ].filter(Boolean),
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-      contentBase: './dist',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          exclude: '/node_modules/',
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader'
-          ],
-        },
-      ],
-    },
-  }
+module.exports = {
+  entry: './src/index.js',
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: '/node_modules/',
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.(jpg)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
