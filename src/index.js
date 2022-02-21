43d52d845a480d3ffeed072b1ad230e2da0908bd
Lee Connelly <lee@leeconnelly.dev>
2022-02-21 19:15:37 +0000

    Added Plenish

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import Stadium from './images/projects/stadium.jpg'
 import GasData from './images/projects/gasdata.jpg'
 import Fitzgerald from './images/projects/fitzgerald.jpg'
 import ClubGlobal from './images/projects/clubglobal.jpg'
+import Plenish from './images/projects/plenish.jpg'
 
 document.getElementById('atoz').src = Atoz
 document.getElementById('stadium').src = Stadium
 document.getElementById('gasData').src = GasData
 document.getElementById('fitzgerald').src = Fitzgerald
 document.getElementById('clubGlobal').src = ClubGlobal
+document.getElementById('plenish').src = Plenish
 
 document.getElementById('hamburger').addEventListener('click', function() {
   document.getElementById(this.dataset.opens).classList.toggle('hidden')
